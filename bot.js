4ab5040cb644a7d4c84cd595e63805fc889a7f6f
Mia5aLifa123xNxx <45575591+Mia5aLifa123xNxx@users.noreply.github.com>
2019-03-02 22:26:24 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -2618,7 +2618,7 @@ member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-    const yumz = member.guild.channels.find("name", `sChannel`);
+    const yumz = member.guild.channels.find("name", `${sChannel}`);
      yumz.send(`<@${member.user.id}> joined by <@${inviter.id}>`);
    //  yumz.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
   }); 
