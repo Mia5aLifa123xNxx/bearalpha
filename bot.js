db86c0e52d7ed53254cf77c94694f059a2679a67
Mia5aLifa123xNxx <45575591+Mia5aLifa123xNxx@users.noreply.github.com>
2019-03-23 19:16:15 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -4440,4 +4440,35 @@ ${data}`+"```")
 }
 
 })}});
+client.on('message', message => {
+    var prefix = '§';//هنا البريفيكس
+    if (message.content.startsWith(prefix + 'reject')) {//هنا الأمر
+        if (message.author.bot) return;
+        if (!message.guild) return;
+        let Room = message.guild.channels.find(`name`, 'accept-denied');
+        let user = message.mentions.users.first();
+        let embedreject = new Discord.RichEmbed()/
+        .setColor('RANDOM')//
+        .setAuthor(user.username,user.avatarURL)
+        .setTitle('» `لم يتم قبولك كإداري` :x: ')//هنا تقدر تغير الكلام حق الرسالة
+        .setThumbnail(message.author.avatarURL)
+        Room.sendEmbed(embedreject);
+    }
+});
+ 
+client.on('message', message => {
+    var prefix = '._.';
+    if (message.content.startsWith(prefix + 'تقديم')) {
+        if (message.author.bot) return;
+        if (!message.guild) return;
+        let Room = message.guild.channels.find(`name`, 'التقديمات');//هنا ضع إسم الروم
+        let user = message.mentions.users.first();
+        let embedreject = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setAuthor(user.username,user.avatarURL)
+        .setTitle('» `تم قبولك كإداري` :white_check_mark:  ')
+        .setThumbnail(message.author.avatarURL)
+        Room.sendEmbed(embedreject);
+    }
+});//By MrX_Dev && EditeD By LionDev_Dz
 client.login(process.env.BOT_TOKEN)
