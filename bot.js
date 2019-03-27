30105d7ed77875c3f1417d95ce4acfdba1e74f16
Mia5aLifa123xNxx <45575591+Mia5aLifa123xNxx@users.noreply.github.com>
2019-03-27 21:48:31 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -2580,127 +2580,99 @@ client.on('messageUpdate', (oldRebel, newRebel) => {
            newRebel.author.send("ممنوع روآبط الدسكورد. \n إذآ كنت تريد النشر توآصل من الإدآرة.");
     }
 });
-var moment = require ("moment")
-var dat = JSON.parse("{}");
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+
+let sWlc = JSON.parse(fs.readFileSync("./setWlc.json", "UTF8"))   
+client.on('message', message => {
+if(message.channel.type === "dm") return;
+if(message.author.bot) return;
+  if(!sWlc[message.guild.id]) sWlc[message.guild.id] = {
+    channel: "welcome"
 }
-//Tropy server codes
-client.on('guildMemberAdd', member => {
-	let welcomer = member.guild.channels.find("name","welcome👋");
-      if(!welcomer) return;
+const channel = sWlc[message.guild.id].channel
+  if (message.content.startsWith(prefix + "setwelcomer")) {
+    if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
+    let newChannel = message.content.split(' ').slice(1).join(" ")
+    if(!newChannel) return message.reply(`**${prefix}setwelcomer <channel name>**`)
+    sWlc[message.guild.id].channel = newChannel
+    message.channel.send(`**${message.guild.name}'s channel has been changed to ${newChannel}**`);
+  }
+   fs.writeFile('./setWlc.json', JSON.stringify(sWlc), (err) => {
+if (err) console.error(err);
+})
+});
+client.on("guildMemberAdd", member => {
+      if(!sWlc[member.guild.id]) sWlc[member.guild.id] = {
+    channel: "welcome"
+  }
+  const channel = sWlc[member.guild.id].channel
+    const sChannel = sWlc[member.guild.id].channel
+    let welcomer = member.guild.channels.find('name', sChannel);
+    let memberavatar = member.user.avatarURL
+      if (!welcomer) return;
       if(welcomer) {
-         moment.locale('ar-ly');
-         var h = member.user;
-        let norelden = new Discord.RichEmbed()
-        .setColor('#070000')
-        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
-         .addField(': تاريخ دخولك السيرفر',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} \n\`\`${moment(member.joinedAt).startOf(' ').fromNow()}\`\``, true)    
-	               .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
-	.addField('📢 | نورت السيرفر يا قلبي' , `Welcome to the server, ${member}`)
-     welcomer.send({embed:norelden});          
-               
- 
-      }
+member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const yumz = member.guild.channels.find("name", `${sChannel}`);
+     yumz.send(`<@${member.user.id}> joined by <@${inviter.id}>`);
+   //  yumz.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+  }); 
+      var Canvas = require('canvas')
+      var jimp = require('jimp')
       
-const channel = member.guild.channels.find("name","welcome👋");
-if (member.user.bot) return;
-var Canvas = require("canvas-prebuilt")
-var jimp = require('jimp')
-  var currentTime = new Date(),
-hours = currentTime.getHours() + 3 ,
-minutes = currentTime.getMinutes(),
-seconds = currentTime.getSeconds();
-if (minutes < 10) {
-minutes = "0" + minutes;
-}
-var suffix = "AM";
-if (hours >= 12) {
-suffix = "PM";
-hours = hours - 12;
-}
-if (hours == 0) {
-hours = 12;
-}
-const w = ['./9.png'];
+      const w = ['./w9.png'];
+      
               let Image = Canvas.Image,
-                  canvas = new Canvas(557, 241),
+                  canvas = new Canvas(400, 200),
                   ctx = canvas.getContext('2d');
+  
               fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
                   if (err) return console.log(err)
                   let BG = Canvas.Image;
                   let ground = new Image;
                   ground.src = Background;
-                  ctx.drawImage(ground, 0, 0, 557, 241);
-
+                  ctx.drawImage(ground, 0, 0, 400, 200);
+      
       })
+      
                       let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
                       jimp.read(url, (err, ava) => {
                           if (err) return console.log(err);
                           ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
                               if (err) return console.log(err);
-
-                                    ctx.font = '20px Arial Bold';
-                              ctx.fontSize = '10px';
-                              ctx.fillStyle = "#FFFFFF";
-                                ctx.fillText(member.user.username, 250, 135);
-
-
-                                              var guild;
-    while (!guild)
-                guild = member.guild
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
-                        member.guild.fetchInvites().then(invs => {
-      let user = Invite.inviter;
-      let invites = invs.filter(i => i.inviter.id === user.id);
-      let inviteCount = invites.reduce((p, v) => v.uses + p, 0);
- channel.send(`**Invited By** : **${Invite.inviter} (${inviteCount})**`);
-
-})
-}
-            dat[Inv] = Invite.uses;
-})
-})
-
-
+      
+                                    ctx.font = "bold 12px Arial";
+                              ctx.fontSize = '20px';
+                              ctx.fillStyle = "#f1f1f1";
+                                ctx.fillText(member.user.username, 200, 150);
+                              
+                              //NAMEً
+                              ctx.font = "bold 12px Arial";
+                              ctx.fontSize = '20px';
+                              ctx.fillStyle = "#f1f1f1";
+      ctx.fillText(`Welcome To Server`, 260, 125);
+      
                               //AVATARً
                               let Avatar = Canvas.Image;
                               let ava = new Avatar;
                               ava.src = buf;
                               ctx.beginPath();
-                 ctx.arc(120.8, 120.5, 112.3, 0, Math.PI*2, true);
-                   ctx.closePath();
-
+                              ctx.arc(77, 101, 62, 0, Math.PI*2);
+                              ctx.stroke();
                                  ctx.clip();
+                                 ctx.drawImage(ava, 13, 38, 128, 126); 
 
-                        ctx.drawImage(ava, 7, 8, 227, 225);
-                              ctx.closePath();
- channel.sendFile(canvas.toBuffer())
-                          })
-})
-});
-   client.on('message',async message => {
-  if(message.content.startsWith(prefix + "voiceonline")) {
-  if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply(':x: **ليس لديك الصلاحيات الكافية**');
-  if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply(':x: **ليس معي الصلاحيات الكافية**');
-  message.channel.send(':white_check_mark:| **تم عمل الروم بنجاح**');
-  message.guild.createChannel(`عدد المتواجدين في روم : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]` , 'voice').then(c => {
-    console.log(`Voice online channel setup for guild: \n ${message.guild.name}`);
-    c.overwritePermissions(message.guild.id, {
-      CONNECT: false,
-      SPEAK: false
-    });
-    setInterval(() => {
-      c.setName(`⟫『 ${message.guild.members.filter(m => m.voiceChannel).size} عدد اعضاء متواجدين في روم 』⟪`)
-    },1000);
-  });
-  }
-});
+                            
+    welcomer.sendFile(canvas.toBuffer())
+      
+      
+                          
+      })
+      })
+      
+      }
+      });
 client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
