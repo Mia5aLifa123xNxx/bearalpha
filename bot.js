d1c46413cfb25cc031cf05bc36aa97d44082ad01
Mia5aLifa123xNxx <45575591+Mia5aLifa123xNxx@users.noreply.github.com>
2019-04-03 15:03:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4556,25 +4556,6 @@ if (message.content.startsWith(prefix + 'tpoint')) {
 }
   
 });
-const moment = require("moment")
-const fs = require("fs")
-
-client.on("guildMemberAdd", member => {
-let welcomer = member.guild.channels.find('name', `welcome`)
-let memberavatar = member.user.avatarURL
-if (!welcomer) return;
-if(welcomer) {
-moment.locale('ar-ly');
-var mrx = member.user;
-let mrxembed = new Discord.RichEmbed()
-.setTitle(mrx.username,`#${mrx.discriminator}`)
-.addField('» Joined Discord ago',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
-.addField('» Your Number',`${member.guild.memberCount}`,true)
-.setThumbnail(mrx.avatarURL)
-.setFooter("Zombie Games.")
-welcomer.send({embed:mrxembed});          
-}})
-
 client.on('guildMemberAdd', member => {
 member.guild.fetchInvites().then(guildInvites => {
 const ei = invites[member.guild.id];
@@ -4586,80 +4567,78 @@ if(!logChannel) return;
 setTimeout(() => {
 logChannel.send(`Invited By: || <@${inviter.id}> ||`);
 },2000)
-});
-});
-
-client.on('guildMemberAdd',async member => {
-const Canvas = require('canvas');
-const jimp = require('jimp');
-const w = ['./welcome_4.png'];
-let Image = Canvas.Image,
-    canvas = new Canvas(800, 300),
-    ctx = canvas.getContext('2d');
-ctx.patternQuality = 'bilinear';
-ctx.filter = 'bilinear';
-ctx.antialias = 'subpixel';
-ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
-ctx.shadowOffsetY = 2;
-ctx.shadowBlur = 2;
-ctx.stroke();
-ctx.beginPath();
-
-fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-    if (err) return console.log(err);
-    let BG = Canvas.Image;
-    let ground = new Image;
-    ground.src = Background;
-    ctx.drawImage(ground, 0, 0, 800, 300);
-
 })
-
-        let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
-        jimp.read(url, (err, ava) => {
-            if (err) return console.log(err);
-            ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-         if (err) return console.log(err);
-
-  ctx.font = '36px Arial';
-  ctx.fontSize = '72px';
-  ctx.fillStyle = "#ffffff";
-  ctx.textAlign = "center";
-  ctx.fillText(member.user.username, 545, 177);
- 
-  ctx.font = '16px Arial Bold';
-  ctx.fontSize = '72px';
-  ctx.fillStyle = "#ffffff";
-  ctx.textAlign = "center";
-  ctx.fillText(`${member.guild.memberCount} Members`, 580, 200);
- 
-  let Avatar = Canvas.Image;
-  let ava = new Avatar;
-  ava.src = buf;
-  ctx.beginPath();
-  ctx.arc(169.5, 148, 126.9, -100, Math.PI * 2, true);
-  ctx.closePath();
-  ctx.clip();
-  ctx.drawImage(ava, 36, 21, 260, 260);
-   
-  let c = member.guild.channels.find('name', `welcome`)
-  if(!c) return;
-  c.sendFile(canvas.toBuffer());
-
 });
-});
-});
+client.on("guildMemberAdd", member => {
+      if(!sWlc[member.guild.id]) sWlc[member.guild.id] = {
+    channel: "welcome"
+  }
+    const channel = sWlc[member.guild.id].channel
+    let welcomer = member.guild.channels.find("name", `welcome`)
+    let memberavatar = member.user.avatarURL
+      if (!welcomer) return;
+      if(welcomer) {
+member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const yumz = member.guild.channels.find("name", `invite-by`)
+     yumz.send(`<@${member.user.id}> joined by <@${inviter.id}>`)
+     yumz.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+  }); 
+      var Canvas = require('canvas')
+      var jimp = require('jimp')
+      
+      const w = ['./w9png'];
+      
+              let Image = Canvas.Image,
+                  canvas = new Canvas(400, 200),
+                  ctx = canvas.getContext('2d');
+  
+              fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+                  if (err) return console.log(err)
+                  let BG = Canvas.Image;
+                  let ground = new Image;
+                  ground.src = Background;
+                  ctx.drawImage(ground, 0, 0, 400, 200);
+      
+      })
+      
+                      let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
+                      jimp.read(url, (err, ava) => {
+                          if (err) return console.log(err);
+                          ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                              if (err) return console.log(err);
+      
+                                    ctx.font = "bold 12px Arial";
+                              ctx.fontSize = '20px';
+                              ctx.fillStyle = "#f1f1f1";
+                                ctx.fillText(member.user.username, 200, 150);
+                              
+                              //NAMEً
+                              ctx.font = "bold 12px Arial";
+                              ctx.fontSize = '20px';
+                              ctx.fillStyle = "#f1f1f1";
+      ctx.fillText(`Welcome To Server`, 260, 125);
+      
+                              //AVATARً
+                              let Avatar = Canvas.Image;
+                              let ava = new Avatar;
+                              ava.src = buf;
+                              ctx.beginPath();
+                              ctx.arc(77, 101, 62, 0, Math.PI*2);
+                              ctx.stroke();
+                                 ctx.clip();
+                                 ctx.drawImage(ava, 13, 38, 128, 126); 
 
-const invites = {};
-
-const wait = require('util').promisify(setTimeout);
-
-client.on('ready', () => {
-wait(1000);
-
-client.guilds.forEach(g => {
-g.fetchInvites().then(guildInvites => {
-invites[g.id] = guildInvites;
-});
-});
-});
+                            
+    welcomer.sendFile(canvas.toBuffer())
+      
+      
+                          
+      })
+      })
+      
+      }
+      });
 client.login(process.env.BOT_TOKEN)
