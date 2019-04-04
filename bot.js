28eb2ba3b66f0860b2cbe6964c7468879bc349c3
Mia5aLifa123xNxx <45575591+Mia5aLifa123xNxx@users.noreply.github.com>
2019-04-04 22:13:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4591,7 +4591,7 @@ client.on('guildMemberAdd', member => {
                
                   .addField("Name:",`<@` + `${member.id}` + `>`, true)
                      
-                                     .addField(``الـسيرفر``, `${member.guild.name}`,true)
+                                     .addField('🌐 | **الـسيرفر**', `${member.guild.name}`,true)
                                        
      .setFooter(`${member.guild.name}`)
         .setTimestamp()
