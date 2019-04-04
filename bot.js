393ed9d5fea6c3a5eda15d30af9487abd2a2fa0b
Mia5aLifa123xNxx <45575591+Mia5aLifa123xNxx@users.noreply.github.com>
2019-04-04 23:37:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3258,8 +3258,7 @@ client.on('ready', () => {//new ready event
                       role.edit({color : "RANDOM"});
                   };
       });
-  }, 1
-7000);//the rainbow time
+  }, 5000);//the rainbow time
 })
 client.on("message", (message) => {
    if (message.content.startsWith("._.new")) {     
