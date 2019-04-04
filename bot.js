a8c17a418653269286bd52503e904be7c20f4a8e
Mia5aLifa123xNxx <45575591+Mia5aLifa123xNxx@users.noreply.github.com>
2019-04-04 16:30:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4577,4 +4577,25 @@ client.on('message',async message => {
         },3000);
     }
 });
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', '📌❨ωєℓcσмє❩💞');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return;
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField('🎽 | name :  ',`${member}`)
+        .addField('📢 | نورت السيرفر يا قلبي' , `Welcome to the server, ${member}`)
+        .addField('🆔 | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                     
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                       
+     .setFooter(`${member.guild.name}`)
+        .setTimestamp()
+   
+      channel.sendEmbed(embed);
+    });
 client.login(process.env.BOT_TOKEN)
