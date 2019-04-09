6992d3157edcfadefa2501c3559338195dbc52df
Mia5aLifa123xNxx <45575591+Mia5aLifa123xNxx@users.noreply.github.com>
2019-04-09 23:17:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -221,7 +221,7 @@ message.channel.send(`**${message.author.username}, your :credit_card: balance i
 }
 });
 client.on('message', async message => {
-    let amount = 250;
+    let amount = 4456421;
     if(message.content.startsWith(prefix + "daily")) {
     if(message.author.bot) return;
     if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
@@ -4601,114 +4601,4 @@ client.on('guildMemberAdd', member => {
    
       channel.sendEmbed(embed);
     });
-const Discord = require("discord.js");
-const fs = require("fs");
-const bot = new Discord.Client();
-const pretty = require("pretty-ms");
-const moment = require("moment");
-const botconfig = JSON.parse(fs.readFileSync('./botconfig.json', 'utf8'));
-const aprefix = "#"
-let gamesTime = 15000;
-
-bot.login(botconfig.token);
-const dailies = require("./dailies.json")
-const sqlite3 = require("sqlite3");
-const sql = new sqlite3.Database("./edited.sqlite");
-
-bot.on('ready', () => {
-  console.log(`On Know`)
-})
-bot.on('message', message => {
-  if (message.channel.type == "dm") return console.log(message.author.username + ` => type this (${message.content}) in Dm`);
-  if (message.author.bot) return;
-
-
-  // T E X T - S Y S T E M //
-  function getUserData(id) {
-    sql.get(`select * from scores where userId = "${id}"`).then(row => {
-      return row;
-    }).catch(e => {
-      sql.run(`INSERT INTO scores (
-      userId, winNu ,
-      loseNu,gamesNu,
-      pointsFkk,points3oasm,
-      pointslogo,pointsimoje,
-      pointssr3h,pointsa3lam,
-      xp , credit,
-      level , like,
-      ane , background,
-      vcpoints, vclevel,
-      guildId, xpguild
-      ) VALUES (? ,? ,? ,? ,? ,? ,? ,? ,? ,?,?,?,?,?,?,?,?, ?, ? , ?)`, [message.author.id, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, '$عني', 'default', 1, 0, message.guild.id, 0]);
-      return sql.get(`select * from scores where userId = "${id}"`);
-    });
-  }
-  bot.on('message', message => {
-    if (message.channel.type == "dm") return console.log(message.author.username + ` => type this (${message.content}) in Dm`);
-    if (message.author.bot || message.system) return;
-    /* T E X T - S Y S T E M */
-    if (message.content.startsWith("d") || message.content.startsWith("هدية")) {
-      let cooldown = 8.64e+7;
-      let lastDaily = dailies[message.author.id]
-      if (lastDaily !== null && cooldown - (Date.now() - lastDaily) > 0) {
-        let timeObj = (cooldown - (Date.now() - lastDaily));
-        message.channel.send(`**:stopwatch: |  ${message.author.username}, هناك مدة زمنية محددة للحصول علي هدية جديدة :  \`${pretty(timeObj, {verbose:true})}\`**`)
-      } else {
-        const amount = 15215231;
-        dailies[message.author.id] = Date.now();
-        sql.run(`UPDATE scores SET credit = ${row.credit + amount} WHERE userId = ${message.author.id}`);
-        message.channel.send(`**:atm: | ${message.author.username}, you received your :yen:${amount}daily credits**`)
-        fs.writeFile("./dailies.json", JSON.stringify(dailies, null, 2), (err) => {
-          if (err) console.log(err);
-        });
-      };
-    };
-    if (message.content.startsWith("c") || message.content.startsWith("c")) {
-      let row = getUserData(message.author.id);
-      if (message.mentions.users.size < 1) {
-        return message.channel.send(` **${message.author.username}, your :credit_card: balance is** ``$${row.credit}`` `).catch(error => message.channel.send(`**:white_check_mark: | تم تفعيل حسابك البنكي **`));
-      } else {
-        let transferto = message.mentions.users.first();
-        if (transferto.bot) return message.channel.send(`**:robot:  |  ${message.author.username}, البوتات لا تمتلك ليفيل وكريديت . :wink:**`);
-        if (transferto.id === message.author.id) return message.channel.send(`من جدك ؟`);
-        let transfer = message.content.split(" ").slice(2).join("");
-        if (row.credit < transfer) return message.channel.send(`**:thinking: | ${message.author.username}, ليس لديك المال الكافي لذلك**`)
-        //getting data from database for the target
-        let transfertoRow = getUserData(transferto.id);
-        if (!transfer) return message.channel.send(`**:credit_card:  |  ${transferto.username}, لديك : ` + "``$" + `${transfertoRow.credit} ` + "``" + ` من الكريديت**`);
-        if (isNaN(transfer) || transfer < 1) return message.channel.send("**:x: | يرجي أدخال رقم صحيح **");
-        //awaiting for the confirm message
-        function codeGen() {
-          return Math.floor(Math.random() * (9 - 0 + 1) + 0);
-        }
-        let code = `${codeGen()}${codeGen()}${codeGen()}${codeGen()}`;
-        message.channel.send(`**${transfer}, Transfer Fees:
-        type these numbers to confirm : \`\`\`${code}\`\`\``).then(codeMessage => {
-          message.channel.awaitMessages(m => m.author.id === message.author.id, {
-            time: 20000,
-            max: 1,
-            errors: ["time"]
-          }).then(messages => {
-            let msg = messages.first();
-            codeMessage.delete();
-            if (msg.content !== code) return message.channel.send(`خطأ`);
-            //transfering the money 
-            sql.run(`UPDATE scores SET credit = credits - ${parseInt(transfer)} WHERE userId = ${message.author.id}`);
-            sql.run(`UPDATE scores SET credit = credits + ${parseInt(transfer)} WHERE userId = ${transferto.id}`);
-            //sending messages
-            message.channel.send(` **➡  |  تمت عملية التحويل  
-          🕊 |  ${transferto} : ${message.author.username}, لقد قمت بأرسال  ` + "$`" + transfer + "`" + ` لـ ** `)
-            transferto.send(`**:atm: | تمت عملية التحويل** \`\`\`\n لقد حولت : ${transfer}  لـ  ${message.author.username} . (ID: ${message.author.id})\`\`\``)
-            bot.channels.get("550305753488031764").send(`ايدي المرسل : \`${transferto.id}\`
-          اسم المستقبل : \`${transferto.username}\`
-          ايدي المستقبل : \`${message.author.id}\`
-          اسم المرسل : \`${message.author.username}\`
-          المبلغ : \`${transfer}\`
-          `);
-          })
-        });
-      };
-    };
-  });
-})
 client.login(process.env.BOT_TOKEN)
