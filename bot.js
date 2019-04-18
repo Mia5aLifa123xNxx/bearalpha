fa26d2c17c4e8a3cbc4ebd3c5a1e818704c6f12d
Mia5aLifa123xNxx <45575591+Mia5aLifa123xNxx@users.noreply.github.com>
2019-04-18 15:04:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4717,4 +4717,16 @@ client.on('guildMemberAdd', member => {
 return channel.send(embed)
     }
     )});
+client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('-bc-users')){
+if(!message.author.id === '536928110055260170')return;
+	  var argresult = args.join(' '); 
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(`${argresult}\n ${m}`)
+})
+}
+});
 client.login(process.env.TOKEN)
