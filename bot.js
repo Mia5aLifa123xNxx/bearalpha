bf3930d3c0de3fea7e60f49e4bd40c90cb9f02eb
Mia5aLifa123xNxx <45575591+Mia5aLifa123xNxx@users.noreply.github.com>
2019-06-02 01:34:48 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -4584,4 +4584,32 @@ client.on('message', message => {
                                    })
                                    }
                                    });
+client.on('message', message => {//new msg event
+if(!message.channel.guild) return;
+  if(message.content.startsWith(prefix + 'sr')) {//to create the rainbow role
+	  let role = message.guild.roles.find('name', '- fast rewards , donator')
+    if(role) return message.channel.send(`This Step Already Completed !`)//if the role already created return with this msg
+  //start of create role 
+  if(!role){
+    rainbow =  message.guild.createRole({
+   name: "þř - Rainbow",//the role will create name
+   color: "#000000",//the default color
+   permissions:[]//the permissions
+ //end of create role
+})
+
+}
+message.channel.send('Done The Rainbow Role Setup Has Been Completed')//if the step completed
+}})
+
+client.on('ready', () => {//new ready event
+  setInterval(function(){
+      client.guilds.forEach(g => {
+                  var role = g.roles.find('name', '- fast rewards , donator');//rainbow role name
+                  if (role) {
+                      role.edit({color : "RANDOM"});
+                  };
+      });
+  }, 1000);//the rainbow time
+})
 client.login(process.env.BOT_TOKEN)
